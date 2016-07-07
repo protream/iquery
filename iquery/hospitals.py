eee00c4f52526837d357bba9f40f5c04c18816bc
protream <protream@gmail.com>
2016-07-07 15:35:08 +0800

    Optimized program logic

--- iquery/hospitals.py
+++ iquery/hospitals.py
@@ -34,6 +34,12 @@ class HospitalCollection(object):
         self._params = params
         self._city = self._params[0]
 
+        if len(self._params) == 2:
+            #: User input a hospital name after city
+            self._hospital = self._params[1]
+        else:
+            self._hospital = ''
+
     @property
     def putian_hospitals_in_city(self):
         hospitals = self._rows.get(self._city, None)
@@ -45,34 +51,54 @@ class HospitalCollection(object):
 
         pt = PrettyTable([self._city])
 
-        l = len(self._params)
-
-        if l == 1:
+        if not self._hospital:
             pt._set_field_names([self._city])
             for hospital in self.putian_hospitals_in_city:
-                color = colored.red
-                pt.add_row([color(hospital) + '\n'])
+                pt.add_row([colored.green(hospital) + '\n'])
             print(pt)
 
-        if l == 2:
-            # User input hospital name
-            h = self._params[1]
-
-            is_putian, field_name = False, self._city + h
+        else:
+            is_putian, field_name = False, self._city + self._hospital
 
             for hospital in self.putian_hospitals_in_city:
-                if h in hospital:
+                if self._hospital in hospital:
                     is_putian, field_name = True, hospital
                     break
 
             pt._set_field_names([field_name])
-            color = colored.red if is_putian else colored.green
-            pt.add_row([color(str(is_putian))])
+            pt.add_row([colored.green(str(is_putian))])
             print(pt)
 
 
 def query(params):
-    """`params` is a `hospital` name  or a `city` name."""
+    """`params` is a city name or a city name + hospital name.
+
+    CLI:
+
+        1. query all putian hospitals in a city:
+
+        $ iquery -p 南京
+        +------+
+        | 南京 |
+        +------+
+        |...   |
+        +------+
+        |...   |
+        +------+
+        ...
+
+
+        2. query if the hospital in the city is putian
+           series, you can only input hospital's short name:
+
+        $ iquery -p 南京 曙光
+        +------------+
+        |南京曙光医院|
+        +------------+
+        |    True    |
+        +------------+
+
+    """
 
     try:
         r = requests.get(QUERY_URL)
